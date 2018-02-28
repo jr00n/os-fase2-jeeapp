158674a4a1315579351d347c654ca01111c49b0f
Jeroen Wolff <jeroen.wolff@gmail.com>
2018-02-28 22:07:25 +0100

    Parallel nu op 2 nodes tegelijk

--- Jenkinsfile-full.groovy
+++ Jenkinsfile-full.groovy
@@ -18,47 +18,50 @@ try {
         node("jos-m3-openjdk8") {
             stage("Git Checkout") {
                 git branch: 'master', url: 'ssh://git@git.belastingdienst.nl:7999/~wolfj09/os-fase2-jeeapp.git'
+                stash name: src, includes: "src"
             }
             stage("Build WAR") {
                 sh "${mvnCmd} clean package -Popenshift"
-                //stash name: "war", includes: "target/ROOT.war"
             }
             stage("Build Image") {
-                //unstash name:"war"
                 sh "oc start-build ${appName}-docker --from-file=target/ROOT.war -n ${project}"
                 openshiftVerifyBuild bldCfg: "${appName}-docker", namespace: project, waitTime: '20', waitUnit: 'min'
             }
             stage("Deploy Image") {
                 openshiftDeploy deploymentConfig: appName, namespace: project
             }
+        }
+        stage ('Test and Analysis') {
+            parallel(
+                'UI Testing': {
+                    node("jos-m3-openjdk8"){
+                        unstash name: src
+                        // service discovery..app
+                        def appURL = sh(script: ocCmd + " get routes -l app=${appName} -o template --template {{range.items}}{{.spec.host}}{{end}}", returnStdout:true)
+                        // service discovery..selenium Hub
+                        def seleniumHubURL = sh(script: ocCmd + " get routes -l app=selenium-grid -o template --template {{range.items}}{{.spec.host}}{{end}}", returnStdout:true)
+                        seleniumHubURL = "http://" + seleniumHubURL + "/wd/hub"
 
-            stage ('Test and Analysis') {
-                parallel(
-                        'UI Testing': {
-                            // service discovery..app
-                            def appURL = sh(script: ocCmd + " get routes -l app=${appName} -o template --template {{range.items}}{{.spec.host}}{{end}}", returnStdout:true)
-                            // service discovery..selenium Hub
-                            def seleniumHubURL = sh(script: ocCmd + " get routes -l app=selenium-grid -o template --template {{range.items}}{{.spec.host}}{{end}}", returnStdout:true)
-                            seleniumHubURL = "http://" + seleniumHubURL + "/wd/hub"
-
-                            sh(script: "mvn integration-test -Pintegration-test -Dseleniumhuburl=${seleniumHubURL} -Dseleniumtesturl=http://${appURL}")
-                            archiveArtifacts artifacts: 'target/screeenshot.png'
-                        },
-                        'Static Analysis': {
-                            sh "${mvnCmd} sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -DskipTests=true"
-                        }
-                )
-            }
-            stage('Sanity check') {
-                steps {
-                    input "Does the staging environment look ok?"
+                        sh(script: "mvn integration-test -Pintegration-test -Dseleniumhuburl=${seleniumHubURL} -Dseleniumtesturl=http://${appURL}")
+                        archiveArtifacts artifacts: 'target/screeenshot.png'
+                    }
+                },
+                'Static Analysis': {
+                    node("jos-m3-openjdk8") {
+                        unstash name: src
+                        sh "${mvnCmd} sonar:sonar -Dsonar.host.url=http://sonarqube:9000 -DskipTests=true"
+                    }
                 }
+            )
+            }
+        }
+        node(){
+            stage('Sanity check') {
+                input "Does the staging environment look ok?"
             }
 
             stage('Deploy - Production') {
-                steps {
                     sh 'echo implement'
-                }
             }
         }
     }
